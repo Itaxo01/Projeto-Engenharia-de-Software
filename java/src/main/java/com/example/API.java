1fb6e31547750e1194e2b63d65e016a53bf7a206
Kauan Fank <kauan.fank@gmail.com>
2025-09-03 10:03:08 -0300

    a

--- java/src/main/java/com/example/API.java
+++ java/src/main/java/com/example/API.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RunApp {
+public class API {
     public static void main(String[] args) {
-        SpringApplication.run(RunApp.class, args);
+        SpringApplication.run(API.class, args);
     }
 }
