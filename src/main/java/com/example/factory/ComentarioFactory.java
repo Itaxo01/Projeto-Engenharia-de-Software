3c6ec63f9efcbae7af396f4b744651749e21ebf2
Kauan Fank <kauan.fank@gmail.com>
2025-11-24 20:46:12 -0300

    Adicionei o globalModelAttributes e mudei o comentário para ser hard delete

--- src/main/java/com/example/factory/ComentarioFactory.java
+++ src/main/java/com/example/factory/ComentarioFactory.java
@@ -7,10 +7,14 @@ import com.example.model.Professor;
 import com.example.model.Usuario;
 
 public class ComentarioFactory {
-	public static Comentario createComentario(Usuario usuario, String texto, Disciplina disciplina, Professor professor) {
-		return new Comentario(usuario, texto, disciplina, professor);
+	public static Comentario criarComentario(Usuario usuario, String texto, Disciplina disciplina, Professor professor, Comentario comentarioPai) {
+		if (comentarioPai == null) {
+			return new Comentario(usuario, texto, disciplina, professor);
+		} else {
+			return new Comentario(usuario, texto, comentarioPai);
+		}
 	}
-
+	
 	public static Comentario createReply(Usuario usuario, String texto, Comentario comentarioPai) {
 		Comentario resposta = new Comentario(usuario, texto, comentarioPai);
 
